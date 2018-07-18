38f68e70fc6fc6cdac5f4da78ea8b4c2be4781fe
Henry Chong <huashiuanchong@gmail.com>
2018-07-18 19:48:43 +0800

    finish basic replying bot

--- examples/echobot2.py
+++ examples/echobot2.py
@@ -31,8 +31,50 @@ logger = logging.getLogger(__name__)
 # update. Error handlers also receive the raised TelegramError object in error.
 def start(bot, update):
     """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
+    update.message.reply_text('''
+Welcome new KRONOS Fans! 👽👽
 
+Our team (including the founders) is available to answer any questions regarding KRONOS and to keep you updated about our newest developments.
+''')
+
+def kronos(bot, update):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('''
+KRONOS seeks to revolutionize the asset management space by equalizing access to the most exclusive investment strategies globally. We have formed a supportive ecosystem of talented traders, entrepreneurs and marketers to bring our vision to market.
+
+With the KRON token, users can access all of our services from market-making, asset management and block trade execution to market insights. So whether you are a crypto exchange, fund, project or investor, you can do more with Kronos on your side!
+
+''')
+
+def social(bot, update):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('''
+English Telegram: https://t.me/KRONOS_E
+Chinese Telegram: https://t.me/Kronos_C
+Airdrop discussion: https://t.me/KRONOS_Airdrop
+Official announcement: https://t.me/KronosToken
+Facebook: https://www.facebook.com/kronostoken/
+Twitter: https://twitter.com/kronostoken
+LinkedIn: https://www.linkedin.com/company/kronostoken/
+Reddit: https://www.reddit.com/r/kronostoken/
+''')
+
+def admins(bot, update):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('''
+⚡️Official Admins:
+Mark - @markpimentel
+Jack - @jaxonomy
+Ran - @ranyi1115
+Wenwen - @wenwen000
+Jennifer - @wangthewhale
+Joe - @Jscherp
+
+⚡️Community Managers:
+Shawn - @Shawntw
+Zane - @krypto_max
+
+''')
 
 def help(bot, update):
     """Send a message when the command /help is issued."""
@@ -48,21 +90,52 @@ def error(bot, update, error):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, error)
 
+def rules(bot, update):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('''
+• Don't talk about Airdrop
+• We are privately funded, and not onboarding any new capital from the chat room!
+• Please keep discussions around Kronos developments, Solidity development, and things that add value to our ecosystem.
+• Spam advertisements / adult content = immediate ban.
+• Please keep discussions polite and constructive.''')
+
+def wp(bot, update):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('''
+⚡️Official website: https://kronostoken.com/
+
+✨Whitepaper username: kronos
+✨password: KRONOS99
+
+''')
+
+def the_most_awesome_project(bot, update):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('''
+KRONOS for sure!!!!!!!!!!!!🚀🌕
+''')
+
+
 
 def main():
     """Start the bot."""
     # Create the EventHandler and pass it your bot's token.
-    updater = Updater("TOKEN")
+    updater = Updater("678188631:AAFHkfaMpURE6MzBu9ndpyNY5nC8hsBRjAQ")
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-
+    # dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("rules", rules))
+    dp.add_handler(CommandHandler("whatkronos", kronos))
+    dp.add_handler(CommandHandler("socialmedia", social))
+    dp.add_handler(CommandHandler("admins", admins))
+    dp.add_handler(CommandHandler("wp", wp))
+    dp.add_handler(CommandHandler("the_most_awesome_project", the_most_awesome_project))
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    # dp.add_handler(MessageHandler(Filters.text, echo))
 
     # log all errors
     dp.add_error_handler(error)
